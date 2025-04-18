23e81022d441ea3595ec30e7a1f2793351a43615
RafinhasouzaDEV <r.nasci223email02@gmail.com>
2025-04-17 21:48:27 -0300

    ativando botao reiniciar

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function sortear() {
     }
     let resultado = document.getElementById("resultado");
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
-    alterandoStatusBotao()
+    alterandoStatusBotao();
 }
 
 function obterNumeroAleatorio(min, max) {
@@ -36,4 +36,13 @@ function alterandoStatusBotao() {
         botao.classList.remove("container__botao");
         botao.classList.add("container__botao-desabilitado");
     }
+}
+
+function reiniciar() {
+    document.getElementById("quantidade").value = "";
+    document.getElementById("de").value = "";
+    document.getElementById("ate").value = "";
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+
+    alterandoStatusBotao();
 }
